25533508e2bd97f734402ed99b84278d68763e0f
githuber <denis>
2023-12-21 18:41:49 +0300

    commitfirst

---
+++ AmonicProj/LoginForm.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AmonicProj
+{
+    /// <summary>
+    /// Interaction logic for LoginForm.xaml
+    /// </summary>
+    public partial class LoginForm : Window
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit(object sender, RoutedEventArgs e)
+        {
+            LoginForm loginForm = new LoginForm();
+            Environment.Exit(0);
+        }
+
+        private void btnLogin(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
