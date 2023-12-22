2d2a0739f3af3c5dd34dd20bf086a6ba60d5d1e5
Головченко Денис Сергеевич <golovtchenko.d.s@corp.kp11.ru>
2023-12-22 10:08:48 +0300

    edit role window

--- AmonicProj/obj/Debug/net8.0-windows8.0/AmonicProj.AssemblyInfo.cs
+++ AmonicProj/obj/Debug/net8.0-windows8.0/AmonicProj.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmonicProj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+909e5c4996fd3f8cb9f2eb93c3babe8e4a454878")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmonicProj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmonicProj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
